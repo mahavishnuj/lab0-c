704733eec3341b7e005fb4bce07987362081ba5b
mahavi <yujuan476@gmail.com>
2021-03-01 00:12:26 +0800

    Change the free and insert

--- queue.c
+++ queue.c
@@ -13,7 +13,10 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
+    if(!q) return NULL;
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
@@ -22,6 +25,16 @@ void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
+    /* If the queue is empty just return*/
+    if(!q) return;
+    
+    list_ele_t *tmp = q->head;
+    while(q->head){
+        q->head = q->head->next;
+        free(tmp->value);
+        free(tmp);
+        tmp = q->head;
+    }
     free(q);
 }
 
@@ -34,13 +47,39 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if(!q){
+        return false;
+    }
     list_ele_t *newh;
     /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if(!newh){
+        return false //If malloc fail it would return NULL value
+    }
+
+    newh->value = malloc(sizeof(char) * (strlen(s) + 1));
+    /*allocate more than 1 size of string length*/
+    if(!newh->value){
+        free(newh);
+        return false;
+    }
+    memset(newh->, '\0', strlen(s) + 1);
+    strncpy(newh->value, s, strlen(s));
+
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     newh->next = q->head;
     q->head = newh;
+
+    //first time insert
+    if(!q->tail){
+        q->tail = q->head;
+    }else{
+        while(q->tail->next){
+            q->tail = q->tail->next;
+        }
+    }
+    q->size += 1;
     return true;
 }
 
